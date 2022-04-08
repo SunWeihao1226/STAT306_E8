1d6b908dc463fdf63f9850279a2628d4e6d72e57
Weihao Sun <sunweihao2019@163.com>
2022-04-07 20:06:14 -0700

    finish method part

--- src/Rcode.R
+++ src/Rcode.R
@@ -3,28 +3,31 @@ library(tidyverse)
 library(ggplot2)
 library(GGally)
 library(rsample)
+library(InformationValue)
+library(caret)
 # Read the dataset
 data <- read_csv("./data/food_hygiene_rating_data.csv", col_names=TRUE)
 
 # Clean the dataset
-
 data_eda <- data %>% 
   select(LocalAuthorityCode, RatingDate, BusinessType, RatingValue) %>%
+  filter(RatingValue==0 | RatingValue==1 | RatingValue==2 | RatingValue==3 | RatingValue==4 | RatingValue==5) %>%
+  mutate(Pass = ifelse((RatingValue >= 3), 1, 0)) %>%
   mutate(Year = format(RatingDate, "%Y") %>% as.numeric(),
-         Month = format(RatingDate, "%m") %>% as.numeric(), 
+         Month = format(RatingDate, "%m") %>% as.numeric(),
          Day = format(RatingDate, "%d") %>% as.numeric(),
-         BusinessType = BusinessType <- as.factor(BusinessType),
-         RatingValue = RatingValue <- as.factor(RatingValue)) %>%
+         BusinessType = BusinessType <- as.factor(BusinessType)) %>%
   select(-RatingDate) %>%
   drop_na()
 
 summary(data_eda)
 
+# Visualization of the dataset
 png("results/LAC_hist.png",width=800, height=600)
 LAC_hist <- hist(data_eda$LocalAuthorityCode, xlab="Local Authority Code", main="Histogram of Local Authority Code")
 dev.off()
 
-options(repr.plot.width = 15, repr.plot.height = 10)
+options(repr.plot.width = 15, repr.plot.height = 8)
 ggplot(data=data_eda, aes(x=factor(BusinessType))) +
   geom_bar(stat="count") +
   xlab("Count") +
@@ -34,18 +37,42 @@ ggplot(data=data_eda, aes(x=factor(BusinessType))) +
   coord_flip()
 ggsave("./results/BT_bar.png")
 
-ggplot(data=data_eda, aes(x=factor(RatingValue))) +
+ggplot(data=data_eda, aes(x=factor(Pass)), height=3) +
   geom_bar(stat="count") +
   xlab("Count") +
   ylab("Rating Value")+
-  ggtitle("Bar Plot of Rating Value")+
-  theme(plot.title = element_text(hjust = 0.5))
+  ggtitle("Bar Plot of Passing")+
+  theme(plot.title = element_text(hjust = 0.5))+
+  coord_flip()
 ggsave("./results/RV_bar.png")
+
+
+# Split the dataset into training and testing sets
 
 data_split <- initial_split(data_eda, prop = 0.75, strata = RatingValue)
 training_data <- training(data_split)
 testing_data <- testing(data_split)
+test_X <- testing_data %>%
+    select(-Pass)
+true_vals <- testing_data$Pass
 
-mod1 <- glm(RatingValue~LocalAuthorityCode+BusinessType+Year+Month+Day, data=training_data, family="binomial")
+
+# Fitting the models
+
+mod <- glm(Pass~LocalAuthorityCode+BusinessType+Year+Month+Day, data=training_data, family="binomial")
+
+# Model evaluation
+
+mod_sum <- summary(mod)
+pred <- predict(mod, newdata=test_X, type = "response")
+cut_off <- optimalCutoff(true_vals, pred)
+final_predict <- ifelse(pred > cut_off, 1, 0)
+final_predict <- as.factor(final_predict)
+true_vals <- as.factor(true_vals)
+conf <- confusionMatrix(true_vals, final_predict)
+conf_mat <- as.table(conf)
+acc <- as.matrix(as.matrix(conf,what="overall")[c(1,2,3,4,5),])
+write.csv(acc,file="./results/accuracy.csv")
+write.csv(conf_mat, file="./results/conf_mat.csv")
 
 
